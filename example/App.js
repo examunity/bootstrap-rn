25f86b41afab504d38d6256f1dae851e5939898e
Anton Kontarev <anton.kontarev@s2-software.de>
2021-10-05 09:55:21 +0200

    Buttons with all color
    Button outlines noch nicht verfügbar

--- example/App.js
+++ example/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Provider, Text, View, StyleSheet, css, Button } from 'bootstyle';
+import {
+  Provider,
+  Text,
+  View,
+  StyleSheet,
+  css,
+  Button,
+  Colors,
+} from 'bootstyle';
+import COLORS from '../src/components/colors';
 
 StyleSheet.build({});
 
@@ -42,32 +51,39 @@ function App() {
       <View style={styles.container}>
         <Text>Anton</Text>
 
+        <Button text="Primary" color={COLORS.PRIMARY} onPress={() => null} />
         <Button
-          text="Default"
+          text="Secondary"
+          color={COLORS.SECONDARY}
           onPress={() => null}
         />
+        <Button text="Success" color={COLORS.SUCCESS} onPress={() => null} />
+        <Button text="Danger" color={COLORS.DANGER} onPress={() => null} />
+        <Button text="Warning" color={COLORS.WARNING} onPress={() => null} />
+        <Button text="Info" color={COLORS.INFO} onPress={() => null} />
+        <Button text="Light" color={COLORS.LIGHT} onPress={() => null} />
+        <Button text="Dark" color={COLORS.DARK} onPress={() => null} />
 
-        <Button
-          text="Default Outline"
-          outline
-          onPress={() => null}
-        />
+        <Button text="classic primary" color="primary" onPress={() => null} />
+        <Button text="classic warning" color="warning" onPress={() => null} />
+        <Button text="clasic success" color="success" onPress={() => null} />
+
+        <Button text="Default Outline" outline onPress={() => null} />
 
         <Button
           text="Secondary Large"
           size="large"
-          theme="secondary"
+          color="secondary"
           onPress={() => null}
         />
 
         <Button
           text="Secondary Small Disabled"
           size="small"
-          theme="secondary"
+          color="secondary"
           disabled
           onPress={() => null}
         />
-
       </View>
     </Provider>
   );
