2ca22787cf2c37c04ef6c40505591930ec0a4f69
Markus J. Wetzel <markuswetzel@gmx.net>
2022-01-09 02:04:15 +0100

    Update CloseButton

--- src/components/close/CloseButton.js
+++ src/components/close/CloseButton.js
@@ -8,27 +8,73 @@ import Pressable from '../Pressable';
 
 const propTypes = {
   children: PropTypes.node,
+  disabled: PropTypes.bool,
+  // eslint-disable-next-line react/forbid-prop-types
+  style: PropTypes.any,
+  // eslint-disable-next-line react/forbid-prop-types
+  textStyle: PropTypes.any,
 };
 
 const styles = StyleSheet.create({
   '.close': css`
+    // Workaround for missing box-sizing in react native.
+    $additionalHeight: $btn-close-padding-y * 2;
+    $additionalWidth: $btn-close-padding-x * 2;
+
     // box-sizing: content-box;
-    padding-vertical: $btn-close-padding-x;
-    padding-horizontal: $btn-close-padding-y;
-    width: $btn-close-width;
-    height: $btn-close-height;
-    color: $btn-close-color;
+    width: $btn-close-width + $additionalHeight;
+    height: $btn-close-height + $additionalWidth;
+    padding: $btn-close-padding-y $btn-close-padding-x;
     background: transparent;
     border-width: 0; // for button elements
+    // @include border-radius();
     opacity: $btn-close-opacity;
+
+    &:hover {
+      opacity: $btn-close-hover-opacity;
+    }
+
+    &:focus {
+      // outline: 0;
+      // box-shadow: $btn-close-focus-shadow;
+      opacity: $btn-close-focus-opacity;
+    }
+  `,
+  '.close-text': css`
+    color: $btn-close-color;
+
+    // Override <a>'s hover style
+    &:hover {
+      color: $btn-close-color;
+      text-decoration: none;
+    }
+  `,
+  '.close-disabled': css`
+    // pointer-events: none;
+    // user-select: none;
+    opacity: $btn-close-disabled-opacity;
   `,
 });
 
 function CloseButton(props) {
-  const classes = getStyles(styles, ['.close']);
+  const {
+    children,
+    disabled = false,
+    style,
+    textStyle,
+    ...elementProps
+  } = props;
+
+  const classes = getStyles(styles, ['.close', disabled && '.close-disabled']);
+  const textClasses = getStyles(styles, ['.close-text']);
 
   return (
-    <Pressable {...props} style={[classes]}>
+    <Pressable
+      {...elementProps}
+      disabled={disabled}
+      style={[classes, style]}
+      textStyle={[textClasses, textStyle]}
+    >
       <Svg viewBox="0 0 16 16">
         <Path
           fill={StyleSheet.value('btn-close-color')}
