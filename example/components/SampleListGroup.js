7ef80b44cfd2c5a41da1c728eae33bfb6a5817dd
S2 Software GmbH & Co. KG <technik@s2-software.de>
2022-01-12 15:17:35 +0100

    ListGroupItemAction
    TODO: Noch nicht alle css klassen drin z.B. bei Dark Color

--- example/components/SampleListGroup.js
+++ example/components/SampleListGroup.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import { ListGroup, Heading } from 'bootstyle';
+import { ListGroup, Heading, Text } from 'bootstyle';
 
 function SampleListGroup() {
   return (
     <>
-      <Heading size={2}>List group</Heading>
+      <Heading size={2}>List group Item</Heading>
       <ListGroup>
-        <ListGroup.Item>Patrick Item first</ListGroup.Item>
-        <ListGroup.Item>Patrick Item Second</ListGroup.Item>
-        <ListGroup.Item>Patrick Item 3</ListGroup.Item>
-        <ListGroup.Item>Patrick Item 4</ListGroup.Item>
+        <ListGroup.Item active>
+          <Text>I am active</Text>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <Text>Default</Text>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <Text>Default</Text>
+        </ListGroup.Item>
+        <ListGroup.Item disabled>
+          <Text>disabled ?</Text>
+        </ListGroup.Item>
+
+        <ListGroup.Item color="dark">
+          <Text>dark</Text>
+        </ListGroup.Item>
+      </ListGroup>
+
+      <Heading size={2}>List group Item Action</Heading>
+      <ListGroup>
+        <ListGroup.ItemAction color="danger">
+          <Text>Action</Text>
+        </ListGroup.ItemAction>
+        <ListGroup.ItemAction color="danger">
+          <Text>Action</Text>
+        </ListGroup.ItemAction>
+        <ListGroup.ItemAction color="danger">
+          <Text>Action</Text>
+        </ListGroup.ItemAction>
+        <ListGroup.ItemAction color="dark">
+          <Text>Action</Text>
+        </ListGroup.ItemAction>
       </ListGroup>
     </>
   );
--- src/components/list-group/ListGroupItemAction.js
+++ src/components/list-group/ListGroupItemAction.js
@@ -2,27 +2,85 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import StyleSheet from '../../style/StyleSheet';
 import css from '../../style/css';
-import View from '../View';
-import { getStyles } from '../../utils';
+import { getStyles, each } from '../../utils';
+import Pressable from '../Pressable';
+import { THEME_COLORS } from '../../theme/constants';
+import { shadeColor, colorContrast } from '../../theme/functions';
 
 const propTypes = {
   children: PropTypes.node.isRequired,
-  // eslint-disable-next-line react/forbid-prop-types
+  color: PropTypes.oneOf(Object.keys(THEME_COLORS)),
   style: PropTypes.any,
+  active: PropTypes.bool,
+  disabled: PropTypes.bool,
 };
 
 const styles = StyleSheet.create({
-  '.list-group-action': css``,
+  '.list-group-action': css`
+    position: relative;
+    padding: $list-group-item-padding-y $list-group-item-padding-x;
+    color: $list-group-color;
+    background-color: $list-group-bg;
+
+    border: $list-group-border-width solid $list-group-border-color;
+  `,
+  ...each(THEME_COLORS, (color, value) => ({
+    [`.list-group-item-${color}`]: css`
+      background-color: ${value};
+      border-color: ${value};
+
+      &:focus {
+        background-color: ${(t) =>
+          shadeColor(t['list-hover-bg-shade-amount'], value(t))};
+        border-color: ${(t) =>
+          shadeColor(t['list-hover-border-shade-amount'], value(t))};
+      }
+
+      &:hover {
+        background-color: ${(t) =>
+          shadeColor(t['list-hover-bg-shade-amount'], value(t))};
+        border-color: ${(t) =>
+          shadeColor(t['list-hover-border-shade-amount'], value(t))};
+      }
+
+      &:active {
+        color: $list-group-action-active-color;
+        background-color: $list-group-action-active-bg;
+      }
+    `,
+  })),
+  '.active': css`
+    z-index: 2; // Place active items above their siblings for proper border styling
+    color: $list-group-active-color;
+    background-color: $list-group-active-bg;
+    border-color: $list-group-active-border-color;
+  `,
+  '.disabled': css`
+    color: $list-group-disabled-color;
+    background-color: $list-group-disabled-bg;
+  `,
 });
 
 const ListGroupItemAction = React.forwardRef((props, ref) => {
-  const { children, style, ...elementProps } = props;
-  const classes = getStyles(styles, ['.list-group-action']);
+  const {
+    children,
+    color = 'null',
+    disabled = false,
+    active = false,
+    style,
+    ...elementProps
+  } = props;
+  const classes = getStyles(styles, [
+    `.list-group-action`,
+    `.list-group-item-${color}`,
+    active && '.active',
+    disabled && '.disabled',
+  ]);
 
   return (
-    <View {...elementProps} ref={ref} style={[classes, style]}>
+    <Pressable {...elementProps} ref={ref} style={[classes, style]}>
       {children}
-    </View>
+    </Pressable>
   );
 });
 
@@ -30,5 +88,3 @@ ListGroupItemAction.displayName = 'ListGroupItemAction';
 ListGroupItemAction.propTypes = propTypes;
 
 export default ListGroupItemAction;
-
-// TODO: ListGroupItemAction
